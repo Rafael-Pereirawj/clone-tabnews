21aa699464214442ede9c45002346f8e7c430468
Rafael-Pereirawj <rafaelpereirawj@gmail.com>
2025-08-23 18:49:13 +0000

    testes 0909

--- fix-git.sh
+++ fix-git.sh
@@ -9,15 +9,5 @@ git config --local commit.gpgsign false
 git config --local tag.gpgsign false
 git config --local gpg.program ""
 
-echo "✅ Configuração concluída!"
+
 git config --list --show-origin | grep -E "user.name|user.email|gpg|commit.gpgsign|tag.gpgsign"
-EOF
-
-chmod +x fix-git.sh
-
-
-echo "Rodando fix-git.sh automaticamente..."
-
-./fix-git.sh
-
-echo -e "\n# Auto-run Git fix\n./fix-git.sh" >> ~/.bashrc
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Teste 055</h1>
+    return <h1>Teste 055 l</h1>
 }
 
 export default Home;
